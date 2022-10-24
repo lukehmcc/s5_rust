502d3262aab586985c6c3830f2f0afc3404b7f06
covalent <lukehmcc@gmail.com>
2022-10-23 21:43:14 -0400

    node_id code is sorted finally

--- src/modules/node.rs
+++ src/modules/node.rs
@@ -6,9 +6,10 @@ pub mod node {
     }
     impl Node {
         pub async fn start_node(self) {
-            let _tree = sled::open(&self.config.database.path).expect("Open sled");
+            let _sled = sled::open(&self.config.database.path).expect("Open sled");
             let p2p: P2PService = P2PService{
-                config: self.config.clone()
+                config: self.config.clone(),
+                sled: _sled.clone(),
             };
             p2p.init().await;
         }
--- src/modules/p2p.rs
+++ src/modules/p2p.rs
@@ -1,13 +1,14 @@
 pub mod p2p {
     use concat_arrays::concat_arrays;
     use ed25519_dalek::PublicKey;
-    use hex::ToHex;
     use sha2::{Digest, Sha256};
+    use sled::Db;
 
     use crate::modules::Config;
     
     pub struct P2PService {
         pub config: Config,
+        pub sled: Db,
     }
     
     impl P2PService {
@@ -16,11 +17,9 @@ pub mod p2p {
             hasher.update(self.config.keypair.seed.as_bytes());
             let hashed_seed = hasher.finalize();
             let pk:PublicKey = ed25519_dalek::PublicKey::from_bytes(&hashed_seed).expect("Seed should be available");
-            let pk_byte: [u8; 32] = pk.to_bytes();
-            let prefix: [u8; 1] = [0xed];
-            let node_id_bin: [u8; 33] = concat_arrays!(prefix, pk_byte);
-            let tree = sled::open(&self.config.database.path).expect("Open sled");
-            tree.insert("node_id", node_id_bin.to_ascii_lowercase());
+            let prefix: [u8;1] = [0xed];
+            let node_id_bin: [u8; 33] = concat_arrays!(prefix, pk.to_bytes());
+            self.sled.insert("node_id", node_id_bin.to_ascii_lowercase()).unwrap();
         }
     }
 }
